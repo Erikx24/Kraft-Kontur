eebe5215d9968d449994885dee696fc58f23a614
Erikx24 <erikfischer03@gmail.com>
2025-01-20 12:48:06 +0100

    1.1.1

--- app/layout.tsx
+++ app/layout.tsx
@@ -47,6 +47,7 @@ export default function RootLayout({
   return (
     <html lang="de" className="!scroll-smooth">
       <head >
+        <meta name="robots" content="noarchive, noodp, noimageindex" />
         <meta name="theme-color" content="#070709" />
         <meta name="color-scheme" content="dark" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no, viewport-fit=cover" />
